1364d94862b56816b99a02cb84b22e70cfea0240
Ryan Smith <rsmith@molecularmatch.com>
2016-06-08 12:05:05 -0700

    corrected to use proper bucket

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ sed -i "s/\SQL_USER/$SQL_USER/" /opt/src/ensembl-rest/ensembl_rest.conf
 sed -i "s/\SQL_PASSWORD/$SQL_PASSWORD/" /opt/src/ensembl-rest/ensembl_rest.conf
 
 #pull down files used by ensembl from s3
-aws s3 cp s3://mms3bucket/importdata/ensembl_api_84_cache_fasta.tgz ensembl_api_84_cache_fasta.tgz
+aws s3 cp s3://mm-s3-bucket/importdata/ensembl_api_84_cache_fasta.tgz ensembl_api_84_cache_fasta.tgz
 
 #unzip the files from s3 to the proper location
 if [ -e "ensembl_api_84_cache_fasta.tgz" ]
